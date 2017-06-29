8ce355689588f078696d0f059c30e265bf6b68dc
Joshua Henderson <joshua.henderson@microchip.com>
2017-06-29 15:20:13 -0700

    Don't hard code to linuxfb

--- resources/HomeAutomation.sh
+++ resources/HomeAutomation.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/.."
-./HomeAutomation --transitions=100 --sensitivity=2 -platform linuxfb $1 2>/dev/null
+./HomeAutomation --transitions=100 --sensitivity=2 $1 2>/dev/null
